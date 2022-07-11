b5e31c903d5faeb527421fbc3cf1f156174a177b
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-07-10 19:54:24 -0500

    update

--- src/main/java/LoginDao.java
+++ src/main/java/LoginDao.java
@@ -11,14 +11,14 @@ public class LoginDao {
     static Connection con;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
+            makeCon();
     }
 
     public static Connection makeCon() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection(url,username,password);
         Statement statement = con.createStatement();
-        statement.executeUpdate("INSERT INTO albums (artist, album_name, release_date, genre, sales) VALUES('2Ace of Base', '2Pyramid', 2001, '2Pop', 14.5)");
+        statement.executeUpdate("INSERT INTO albums (artist, album_name, release_date, genre, sales) VALUES('2Aceqw3 of Base', '2Pyqwra23mid', 2001, '2P23op', 14.5)");
         return con;
     }
 
