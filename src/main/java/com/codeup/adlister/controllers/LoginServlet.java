1e06bf103bc1fcb6fcf6df66004d9b307f52afc3
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-07-14 16:06:10 -0500

    finished

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,14 +24,19 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String hashpw = BCrypt.hashpw(password, BCrypt.gensalt());
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
         if (user == null) {
             response.sendRedirect("/login");
             return;
         }
+        System.out.println("Printing password comparison");
+        System.out.println(password);
+        System.out.println(user.getPassword());
 
-        boolean validAttempt = password.equals(user.getPassword());
+//        boolean validAttempt = password.equals(user.getPassword());
+        boolean validAttempt = BCrypt.checkpw(password, hashpw);
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
