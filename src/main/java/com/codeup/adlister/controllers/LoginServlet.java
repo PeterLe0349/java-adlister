bbae9002908b792e41ea255df013293dc8b6f3ea
Michael Friend <michael.l.friend2@gmail.com>
2022-07-25 14:41:11 -0500

    completed adding logic

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
@@ -22,16 +23,22 @@ public class LoginServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         User user = DaoFactory.getUsersDao().findByUsername(username);
+        HttpSession session = request.getSession();
         if (user == null) {
             response.sendRedirect("/login");
             return;
         }
 
         boolean validAttempt = Password.check(password, user.getPassword());
+
+        if (user != null && !validAttempt) {
+            request.setAttribute("username", username);
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
